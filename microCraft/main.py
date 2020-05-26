ec413b5291a93f3233abe73c0f04d111c1f5ee88
jan Esili <bismutum31@gmail.com>
2020-05-26 03:52:04 -0400

    Fixed formatting errors and added keep-alive packets.

--- microCraft/main.py
+++ microCraft/main.py
@@ -3,13 +3,14 @@ import random
 from quarry.net.server import ServerProtocol, ServerFactory
 from twisted.internet import reactor
 
-from microCraft.packethandler import packet_dictionary
-
 
 class MainProtocol(ServerProtocol):
 
     random.seed()
-    alive_key = 0
+
+    def __init__(self, factory, remote_addr):
+        super().__init__(factory, remote_addr)
+        self.alive_key = 0
 
     def player_joined(self):
         ServerProtocol.player_joined(self)
@@ -45,21 +46,26 @@ class MainProtocol(ServerProtocol):
         # Announce player joined
         self.factory.send_chat(u"\u00a7e%s has joined." % self.display_name)
 
-        def player_left(self):
-            ServerProtocol.player_left(self)
+    def player_left(self):
+        ServerProtocol.player_left(self)
 
-            # Announce player left
-            self.factory.send_chat(u"\u00a7e%s has left." % self.display_name)
+        # Announce player left
+        self.factory.send_chat(u"\u00a7e%s has left." % self.display_name)
 
-        def keep_alive(self):
-            alive_key = random.getrandbits(32)
-            self.buff_type.pack("l", alive_key)
+    def keep_alive(self):
+        self.alive_key = random.getrandbits(32)
+        self.buff_type.pack("l", self.alive_key)
 
-        def packet_unhandled(self, buff, name):
-            packet_dictionary.get(name, ServerProtocol.packet_unhandled(self, buff, name))(self, buff, alive_key)
-
-
+    def packet_unhandled(self, buff, name):
+        if name == "keep_alive":
+            if buff != self.alive_key:
+                ServerProtocol.close(self, "Invalid keep-alive packet.")
 
 
 class MainFactory(ServerFactory):
     protocol = MainProtocol
+    motd = "microCraft Server"
+
+    def send_chat(self, message):
+        for player in self.players:
+            player.send_packet("chat_message", player.buff_type.pack_chat(message) + player.buff_type.pack('B', 0))
